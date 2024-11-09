b3486add344daca0d770bec24c15eeceecd0a576
Sazal-Abdullah <sazalahamed61@gmail.com>
2024-11-09 10:23:24 +0600

    30

--- resources/views/admin/pages/deals/index.blade.php
+++ resources/views/admin/pages/deals/index.blade.php
@@ -12,7 +12,6 @@
                     {{ session('success') }}
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
-                        
                     </button>
                 </div>
             @endif
